394966fc0a1336619e5d84175ecdd984d235579f
Hemant Ramawat <hemant.ramawat@nyu.edu>
2022-02-14 02:47:11 -0500

    lab1: implemented file reader

--- lab1/src/linker.cpp
+++ lab1/src/linker.cpp
@@ -1,56 +1,39 @@
 #include <iostream>
+#include <stdlib.h>
 #include <string>
+#include <string.h>
 #include <fstream>
 #include <map>
+#include <getopt.h>
 // #include <fmt/format.h>
 
 using namespace std;
 
-/* For file based output
-bool outputToFile = true;
-bool isOutputFileOpen = false;
-string OUTFILE = "output.txt";
-ofstream outFileStream;
-*/
 enum PASS_TYPE {PASS_ONE, PASS_TWO};
 enum ADDR_MODE {RELATIVE, EXTERNAL, IMMEDIATE, ABSOLUTE, ERROR};
 map<string, int> symbolMap;
+string INPUT_FILE = "";
 
-/* void PrintOutput(string str)
+struct Token
 {
-    if (outputToFile)
-    {
-        outFileStream << str;
-    }
-    else
-    {
-        cout << str;
-    }
-} */
+  string value;
+  int lineNumber;
+  int offset;
 
-/* void PerformInitializationTasks()
-{
-    if (outputToFile)
-    {
-        outFileStream.close();
-        outFileStream.open(OUTFILE);
-        isOutputFileOpen = true;
-    }
-} */
+  Token(string _value, int _lineNum, int _offset)
+  {
+    value = _value;
+    lineNumber = _lineNum;
+    offset = _offset;
+  }
+};
 
-/*
-* void PrintSymbolMap()
-* {
-* 
-* }
-*/
+ostream& operator<<(ostream& os, const Token& token) {
+    return os << "Token: " 
+              << token.lineNumber << ":" << token.offset
+              << " : " << token.value;
+}
 
-/*
-* void PrintMemoryMap()
-* {
-* 
-* }
-*/
 
 string getToken()
 {
@@ -86,14 +69,6 @@ pair<ADDR_MODE, string> readIAER()
     }
 }
 
-/* void PerformFinalTasks()
-{
-    if (isOutputFileOpen)
-    {
-        outFileStream.close();
-    }
-} */
-
 void ParseInput(PASS_TYPE passNum)
 {
     // getToken
@@ -101,19 +76,46 @@ void ParseInput(PASS_TYPE passNum)
 
 int main (int argc, char** argv)
 {
-    cout << "Hello, world!" << endl;
-    // PerformInitializationTasks();
     int num = 5;
-    // PrintOutput(fmt::format("The answer is {:05}.", num));
     printf("The answer is %05d.\n", num);
+
+    // Read input file name from the command line arguments
+    if (argc < 2 || argv[1] == "")
+    {
+      printf("Fail: Provide input file name as program argument.");
+      exit(EXIT_FAILURE);
+    }
+
+    INPUT_FILE = string(argv[1]);
+
+    printf("Input file name: %s\n", INPUT_FILE.c_str());
+
+    ifstream fin;
+    fin.open(INPUT_FILE.c_str());
+    string line;
+    while (fin) {
+      // Read a line from input file
+      getline(fin, line);
+
+      char line_cstr[line.length() + 1];
+      strcpy(line_cstr, line.c_str());
+      char* sToken = strtok(line_cstr, " \t\n");
+      
+      cout << line << endl;
+      while (sToken != NULL)
+      {
+        printf ("'%s'\n",sToken);
+        sToken = strtok (NULL, " \t\n");
+      }
+
+    }
+    // Close input file
+    fin.close();
 
     // Parse for Symbol Map i.e. first pass
     ParseInput(PASS_ONE);
-    // PrintSymbolMap();
 
     // Parse second time to update addresses and resolve external references in Memory map
     ParseInput(PASS_TWO);
-    // PrintMemoryMap();
 
-    // PerformFinalTasks();
 }
